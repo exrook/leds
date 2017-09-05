7f893207c5f394c956fd45bb2d82da3dac7f6d90
Jacob Hughes <j@jacobhughes.me>
2017-09-04 22:21:14 -0400

    Added serde support

--- src/bin/stream_neopixels.rs
+++ src/bin/stream_neopixels.rs
@@ -4,43 +4,55 @@ extern crate clap;
 extern crate atomic_box;
 
 use std::time::Duration;
-use std::thread::{spawn,sleep};
+use std::thread::{spawn, sleep};
 use std::thread;
 use std::io::BufRead;
 use std::sync::Arc;
 
-use clap::{App,Arg};
+use clap::{App, Arg};
 
-use set_neopixels::{Pixel,Effect,AuxEffect,set_effect,setup};
+use set_neopixels::{Pixel, Effect, AuxEffect, set_effect, setup};
 use atomic_box::AtomicBox;
 
 fn main() {
     let matches = App::new("set_neopixels")
-        .arg(Arg::with_name("serial").value_name("port").required(true).takes_value(true).index(1))
+        .arg(
+            Arg::with_name("serial")
+                .value_name("port")
+                .required(true)
+                .takes_value(true)
+                .index(1),
+        )
         .get_matches();
     let port = matches.value_of("serial").unwrap().to_owned();
-    let b: Arc<AtomicBox<Option<(Pixel, Effect, Option<Pixel>, AuxEffect)>>> = Arc::new(AtomicBox::new(None));
+    let b: Arc<AtomicBox<Option<(Pixel, Effect, Option<Pixel>, AuxEffect)>>> =
+        Arc::new(AtomicBox::new(None));
     let b2 = b.clone();
     let handle = std::thread::spawn(move || {
         let mut serial = setup(&port);
         thread::park();
         loop {
             match Arc::try_unwrap(*(b.swap(None))) {
-                Ok(Some(l)) => 
-            set_effect(
-                &mut serial,
-                l.0, // color
-                l.1,// effect
-                l.2, // aux_color
-                l.3 // aux_effect
-            ).unwrap(),
+                Ok(Some(l)) => {
+                    set_effect(
+                        &mut serial,
+                        l.0, // color
+                        l.1, // effect
+                        l.2, // aux_color
+                        l.3, // aux_effect
+                    ).unwrap()
+                }
                 _ => {}
             }
+            thread::sleep_ms(20);
         }
     });
     sleep(Duration::from_secs(1));
     let stdin = std::io::stdin();
-    for l in stdin.lock().lines().filter_map(|x| x.ok()).filter_map(string_to_opts) {
+    for l in stdin.lock().lines().filter_map(|x| x.ok()).filter_map(
+        string_to_opts,
+    )
+    {
         println!("{:?}", l);
         b2.store(Some(l));
         handle.thread().unpark();
@@ -52,21 +64,24 @@ fn main() {
 fn string_to_opts<T: AsRef<str>>(s: T) -> Option<(Pixel, Effect, Option<Pixel>, AuxEffect)> {
     let v: Vec<_> = s.as_ref().split_whitespace().collect();
     if v.len() != 10 {
-        return None
+        return None;
     }
-    let color = match v[0..3].iter().map(|x|x.parse().ok()).collect::<Option<Vec<_>>>() {
+    let color = match v[0..3]
+        .iter()
+        .map(|x| x.parse().ok())
+        .collect::<Option<Vec<_>>>() {
         Some(c) => {
             Pixel {
                 red: c[0],
                 green: c[1],
-                blue: c[2]
+                blue: c[2],
             }
         }
-        None => return None
+        None => return None,
     };
     let param = match v[4].parse().ok() {
         Some(s) => s,
-        None => return None
+        None => return None,
     };
     let effect = match v[3] {
         "Constant" => Effect::Constant,
@@ -76,21 +91,24 @@ fn string_to_opts<T: AsRef<str>>(s: T) -> Option<(Pixel, Effect, Option<Pixel>, 
         "DoubleWidth" => Effect::DoubleWidth(param),
         "QuadWidth" => Effect::QuadWidth(param),
         "Edges" => Effect::Edges(param),
-        _ => return None
+        _ => return None,
     };
-    let aux_color = match v[5..8].iter().map(|x|x.parse().ok()).collect::<Option<Vec<_>>>() {
+    let aux_color = match v[5..8]
+        .iter()
+        .map(|x| x.parse().ok())
+        .collect::<Option<Vec<_>>>() {
         Some(c) => {
             Pixel {
                 red: c[0],
                 green: c[1],
-                blue: c[2]
+                blue: c[2],
             }
         }
-        None => return None
+        None => return None,
     };
     let aux_param = match v[9].parse().ok() {
         Some(s) => s,
-        None => return None
+        None => return None,
     };
     let aux_effect = match v[8] {
         "None" => AuxEffect::None,
@@ -100,7 +118,7 @@ fn string_to_opts<T: AsRef<str>>(s: T) -> Option<(Pixel, Effect, Option<Pixel>, 
         "FillRight" => AuxEffect::FillRight(aux_param),
         "FillEdges" => AuxEffect::FillEdges(aux_param),
         "FillDouble" => AuxEffect::FillDouble(aux_param),
-        _ => return None
+        _ => return None,
     };
     Some((color, effect, Some(aux_color), aux_effect))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 extern crate serial;
+#[cfg(feature = "serde")]
+#[macro_use]
+extern crate serde;
 
 use std::io;
 
@@ -10,7 +13,8 @@ use std::ffi::OsStr;
 
 use std::f64::consts::PI;
 
-#[derive(Clone,Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Clone, Debug)]
 pub struct Pixel {
     pub red: u8,
     pub green: u8,
@@ -22,11 +26,12 @@ impl Pixel {
         Pixel {
             red: s[0],
             green: s[1],
-            blue: s[2]
+            blue: s[2],
         }
     }
 }
 
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy, Clone)]
 pub enum Effect {
     Constant,
@@ -35,9 +40,10 @@ pub enum Effect {
     Width(u8),
     DoubleWidth(u8),
     QuadWidth(u8),
-    Edges(u8)
+    Edges(u8),
 }
 
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Copy, Clone)]
 pub enum AuxEffect {
     None,
@@ -66,13 +72,18 @@ pub fn setup<T: AsRef<OsStr> + ?Sized>(port: &T) -> serial::SystemPort {
 }
 
 pub fn set_pixels<T: SerialPort>(port: &mut T, pixels: Vec<Pixel>) -> io::Result<()> {
-    let mut bytes: Vec<u8> = pixels.into_iter().flat_map(|p| {vec!(p.red,p.green,p.blue,0)}).collect();
+    let mut bytes: Vec<u8> = pixels
+        .into_iter()
+        .flat_map(|p| vec![p.red, p.green, p.blue, 0])
+        .collect();
     {
-        let last = bytes.len()-1;
+        let last = bytes.len() - 1;
         bytes[last] = 255;
     }
     try!(port.flush());
-    try!(port.write_all(&vec!(255,255,255,255,108,101,100,122)));
+    try!(port.write_all(
+        &vec![255, 255, 255, 255, 108, 101, 100, 122],
+    ));
     let mut resp = String::new();
     try!(port.read_to_string(&mut resp));
     println!("{}", resp);
@@ -81,7 +92,13 @@ pub fn set_pixels<T: SerialPort>(port: &mut T, pixels: Vec<Pixel>) -> io::Result
     Ok(())
 }
 pub fn set_pixels2<T: SerialPort>(port: &mut T, pixels: Vec<Pixel>) -> io::Result<()> {
-    let bytes: Vec<u8> = pixels.into_iter().enumerate().flat_map(|(i,p)| {vec!('c' as u8,p.red,p.green,p.blue,'l' as u8,i as u8)}).collect();
+    let bytes: Vec<u8> = pixels
+        .into_iter()
+        .enumerate()
+        .flat_map(|(i, p)| {
+            vec!['c' as u8, p.red, p.green, p.blue, 'l' as u8, i as u8]
+        })
+        .collect();
     println!("{:#?}", bytes);
     try!(port.flush());
     try!(port.write_all(&bytes));
@@ -96,10 +113,10 @@ pub fn set_pixels3<T: SerialPort>(port: &mut T, pixels: Vec<Pixel>) -> io::Resul
     try!(port.flush());
     //let chunks = bytes.chunks(3);
     //for (i,chunk) in chunks.enumerate() {
-    for (i,pixel) in pixels.iter().enumerate() {
+    for (i, pixel) in pixels.iter().enumerate() {
         //println!("Writing pixel {}", i);
         //try!(port.write_all(&chunk));
-        try!(port.write_all(&vec!(pixel.red,pixel.green,pixel.blue)));
+        try!(port.write_all(&vec![pixel.red, pixel.green, pixel.blue]));
         sleep(Duration::from_millis(10));
         //sleep(Duration::new(0,300000));
     }
@@ -107,33 +124,42 @@ pub fn set_pixels3<T: SerialPort>(port: &mut T, pixels: Vec<Pixel>) -> io::Resul
 }
 
 pub fn set_pixels4<T: SerialPort>(port: &mut T, pixels: &[Pixel]) -> io::Result<()> {
-    port.write_all(&pixels.into_iter().flat_map(|p|vec![p.red,p.green,p.blue]).collect::<Vec<u8>>())
+    port.write_all(&pixels
+        .into_iter()
+        .flat_map(|p| vec![p.red, p.green, p.blue])
+        .collect::<Vec<u8>>())
 }
 
-pub fn set_effect<T: SerialPort>(port: &mut T, color: Pixel, effect: Effect, color2: Option<Pixel>, aux_effect: AuxEffect ) -> io::Result<()> {
+pub fn set_effect<T: SerialPort>(
+    port: &mut T,
+    color: Pixel,
+    effect: Effect,
+    color2: Option<Pixel>,
+    aux_effect: AuxEffect,
+) -> io::Result<()> {
     let mut bytes = Vec::new();
-    bytes.append(&mut vec!(color.red,color.green,color.blue));
+    bytes.append(&mut vec![color.red, color.green, color.blue]);
     bytes.append(&mut match effect {
-        Effect::Constant => vec!(0,0),
-        Effect::Flash(rate) => vec!(1,rate),
-        Effect::SetPix(num) => vec!(2,num),
-        Effect::Width(width) => vec!(3,width),
-        Effect::DoubleWidth(width) => vec!(4,width),
-        Effect::QuadWidth(width) => vec!(5,width),
-        Effect::Edges(width) => vec!(6,width),
+        Effect::Constant => vec![0, 0],
+        Effect::Flash(rate) => vec![1, rate],
+        Effect::SetPix(num) => vec![2, num],
+        Effect::Width(width) => vec![3, width],
+        Effect::DoubleWidth(width) => vec![4, width],
+        Effect::QuadWidth(width) => vec![5, width],
+        Effect::Edges(width) => vec![6, width],
     });
     bytes.append(&mut match color2 {
-        Some(color2) => vec!(color2.red,color2.green,color2.blue),
-        None => vec!(0,0,0),
+        Some(color2) => vec![color2.red, color2.green, color2.blue],
+        None => vec![0, 0, 0],
     });
     bytes.append(&mut match aux_effect {
-        AuxEffect::None => vec!(0,0),
-        AuxEffect::Offset(amount) => vec!(1, amount),
-        AuxEffect::FillLeft(len) => vec!(2, len),
-        AuxEffect::FillCenter(len) => vec!(3, len),
-        AuxEffect::FillRight(len) => vec!(4, len),
-        AuxEffect::FillEdges(len) => vec!(5, len),
-        AuxEffect::FillDouble(len) => vec!(6, len),
+        AuxEffect::None => vec![0, 0],
+        AuxEffect::Offset(amount) => vec![1, amount],
+        AuxEffect::FillLeft(len) => vec![2, len],
+        AuxEffect::FillCenter(len) => vec![3, len],
+        AuxEffect::FillRight(len) => vec![4, len],
+        AuxEffect::FillEdges(len) => vec![5, len],
+        AuxEffect::FillDouble(len) => vec![6, len],
     });
     println!("Setting effect {:#?}, color: {:?}", effect, color);
     try!(port.write_all(&bytes));
